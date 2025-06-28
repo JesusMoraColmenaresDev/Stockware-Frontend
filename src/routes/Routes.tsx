559966afcf5a9375c5816daa226a2e32b6b3ba59
Jesus Miguel Mora Colmenares <jesusmiguelmora.dev@gmail.com>
2025-06-27 21:27:57 -0400

    modified routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePageView from "../views/HomePageView";
+import CreateProductsView from "../views/CreateProductsView";
 
 export const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePageView/>}></Route>
+                <Route path="/product/create" element={<CreateProductsView/>}></Route>
             </Routes>
         </Router>
     )

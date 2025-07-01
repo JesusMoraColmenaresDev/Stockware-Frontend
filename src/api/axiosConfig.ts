2012b936e9d8e546d93d427623cf65278ba7d929
Pobduard <jeduardoa2003@gmail.com>
2025-07-01 00:55:23 -0400

    .

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,12 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
-    headers: {
-        'Content-Type' : "aplication/json",
-        "Accept": 'aplication/json'
-    },
-    withCredentials: true,
+	baseURL: "http://localhost:3000",
+	headers: {
+		"Content-Type": "aplication/json",
+		Accept: "aplication/json",
+	},
+	withCredentials: false,
 });
 
 export default api;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,14 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePageView from "../views/HomePageView";
 import CreateProductsView from "../views/CreateProductsView";
+import { SideBar } from "../components/SideBar";
+
+const router = createBrowserRouter([
+	{
+		path: "/", //& Ya tiene el / , los hijos no lo requieren
+		element: <SideBar />,
+		children: [
+			{ index: true, element: <HomePageView /> },
+			{ path: "/product/create", element: <CreateProductsView /> },
+		],
+	},
+]);
 
 export const AppRoutes = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePageView/>}></Route>
-                <Route path="/product/create" element={<CreateProductsView/>}></Route>
-            </Routes>
-        </Router>
-    )
-}
+	return <RouterProvider router={router} />;
+};

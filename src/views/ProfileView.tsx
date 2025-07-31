b6ad7801d13c5236454ae3c811f8903b425c0762
Pobduard <jeduardoa2003@gmail.com>
2025-07-31 18:21:00 -0400

    Before Meeting

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -7,11 +7,25 @@ import { DeleteAccountModal } from "../components/users/DeleteAccountModal";
 import { useGetProfileInfo } from "../api/profileApi";
 import { Spinner } from "../components/Spinner";
 import { createDBBackUp } from "../api/usersApi";
+import { useState } from "react";
 
 export default function ProfileView() {
+	const [requestMessage, setrequestMessage] = useState("");
+	const [messageColor, setMessageColor] = useState("");
 	const navigate = useNavigate();
 
 	const { data: profile, isLoading, isError, error } = useGetProfileInfo();
+
+	const handleClick = async () => {
+		const response = await createDBBackUp();
+		if (response) {
+			setMessageColor("text-sucess");
+			setrequestMessage(response);
+		} else {
+			setMessageColor("text-bg-button-delete");
+			setrequestMessage("No response from the server.");
+		}
+	};
 
 	if (isLoading) {
 		return (
@@ -29,10 +43,15 @@ export default function ProfileView() {
 		<>
 			<div className="flex w-full h-full flex-col items-center justify-start p-8 bg-bg-main gap-8">
 				{profile?.role === "admin" && (
-					<div className="flex items-end justify-end w-full">
+					<div className="flex items-center w-full relative ">
+						<div
+							className={`flex-1 flex justify-center ${messageColor} font-bold text-lg p-4 rounded-md`}
+						>
+							{requestMessage}
+						</div>
 						<button
-							className="text-xl font-bold flex items-center gap-[0.5rem] py-[0.5rem] px-[1rem] rounded-md text-white bg-bg-button-primary hover:bg-bg-button-secondary"
-							onClick={() => createDBBackUp()}
+							className="text-xl font-bold flex justify-end text-center items-center gap-[0.5rem] py-[0.5rem] px-[1rem] rounded-md text-white bg-bg-button-primary hover:bg-bg-button-secondary"
+							onClick={() => handleClick()}
 						>
 							Create BackUp
 						</button>
